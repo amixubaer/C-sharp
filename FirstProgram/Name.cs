c16b23c91c95e750d24f8e72a9acd2f7e1bc1122
MD. ABU JUBAER <62588064+amixubaer@users.noreply.github.com>
2021-06-14 09:59:53 +0600

    Add files via upload

---
+++ FirstProgram/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Name
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter Your Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi " + (name));
+            Console.ReadLine();
+        }
+    }
+}
